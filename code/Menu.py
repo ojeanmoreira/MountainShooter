29dcf0a63a8987a3881bc3bacfd79ad2239b6787
oJeanMoreira <jctmj@hotmail.com>
2025-09-01 17:40:08 -0300

    Fixing imports in Level.py file

--- code/Menu.py
+++ code/Menu.py
@@ -10,7 +10,7 @@ class Menu:
     def __init__(self, window):
         pygame.mixer.init()
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
         self.menu_option = 0
 
